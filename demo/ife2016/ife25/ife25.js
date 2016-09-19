cd5963806801d11bc6565f2aa8a5129cb16f7241
haizai <384257538@qq.com>
2016-09-19 18:14:40 +0900

    Site updated: 2016-09-19 18:14:37

--- demo/ife2016/ife25/ife25.js
+++ demo/ife2016/ife25/ife25.js
@@ -19,7 +19,9 @@ Tree.prototype={
   addChild:function(){
     var self = this
     if(this.iOpen.classList.contains("img-close")){this.toggleChilds()}//如果父元素close，则打开它
-    for(var i=0;i<arguments.length;i++) {="" var="" newdiv="document.createElement("DIV");" newdiv.innerhtml="<p><i class="img-open"></i><label>" +arguments[i]+'<="" label=""><i class="img-add"></i><i class="img-delete"></i><p></p>'
+    for(var i=0;i<arguments.length;i++){   
+      var newDiv = document.createElement("DIV");
+      newDiv.innerHTML="<p><i class='img-open'></i><label>" + arguments[i] + "</label><i class='img-add'></i><i class='img-delete'></i></p>"
       newDiv.classList.add("son")
       this.selfNode.appendChild(newDiv)
       self.anime(newDiv,'animeAdd')
@@ -34,11 +36,47 @@ Tree.prototype={
   //删除自身
   delSelf:function(istrue){
     if(istrue){
-      if(this.parent.childs.length===1){//如果是最后一个元素，父级iOpen none
+      //如果是最后一个元素，父级iOpen none
+      if(this.parent.childs.length===1){
         this.parent.iOpenRender(false)
       }
-      for(var i=0;i<this.parent.childs.length;i++){ if(this.parent.childs[i]="==this){" this.parent.childs.splice(i,1)="" 父级移除这个childs="" this.parent.selfnode.removechild(this.selfnode)="" 父级dom移除这个dom="" break="" }="" },="" 展开隐藏子节点="" togglechilds:function(){="" var="" self="this" img="this.iOpen" img.classlist.toggle("img-close")="" this.childs.foreach(function(item){="" if(img.classlist.contains("img-close")){="" item.selfnode.classlist.add('none')="" }else{="" item.selfnode.classlist.remove('none')="" self.anime(item.selfnode,'animeadd')="" })="" i标签切换="" iopenrender:function(isopen){="" if(isopen){="" this.iopen.classlist.remove('none')="" this.iopen.classlist.add('none')="" 渲染被搜索的值="" labelrender:function(isrender,patt){="" if(isrender){="" span加包裹层="" this.label.innerhtml="this.label.innerHTML.replace(patt,'<span" style="text-decoration: underline;color:red">'+patt.source+'')
-    }else{//所有span去包裹层
+      for(var i=0;i<this.parent.childs.length;i++){
+        if(this.parent.childs[i]===this){
+          this.parent.childs.splice(i,1)//父级移除这个childs
+          this.parent.selfNode.removeChild(this.selfNode)//父级dom移除这个dom
+          break   
+        }
+      }
+    }
+  },
+  //展开隐藏子节点
+  toggleChilds:function(){
+    var self = this
+    var img =this.iOpen
+    img.classList.toggle("img-close")
+    this.childs.forEach(function(item){
+      if(img.classList.contains("img-close")){
+        item.selfNode.classList.add('none')
+      }else{
+        item.selfNode.classList.remove('none')
+        self.anime(item.selfNode,'animeAdd')
+      }
+    })
+  },
+  //i标签切换
+  iOpenRender:function(isopen){
+    if(isopen){
+      this.iOpen.classList.remove('none')
+    }else{
+      this.iOpen.classList.add('none')
+    }
+  },
+  //渲染被搜索的值
+  labelRender:function(isRender,patt){
+    //span加包裹层、去包裹层
+    if(isRender){
+      this.label.innerHTML=this.label.innerHTML.replace(patt,'<span style="text-decoration: underline;color:red">'+patt.source+'</span>')
+    }else{
       for(var i=this.label.children.length-1;i>=0;i--){
         this.label.children[i].outerHTML=this.label.children[i].innerHTML
       }
@@ -53,9 +91,53 @@ document.getElementById('searchBtn').onclick=function(){
   var inputValue = document.getElementById('searchText').value
   var patt=new RegExp(inputValue,'g')
   var searchTree
-  searchTreeArr.filter(function(item){//还原，返回false,数组变为[]
+  //还原，返回false,数组变为[]
+  searchTreeArr.filter(function(item){
     item.labelRender(false)
     return false
   })
-  if(inputValue.trim()===""){return}//输入空直接返回
-  for(var i=0;i</this.parent.childs.length;i++){></arguments.length;i++)>
+  if(inputValue.trim()===""){return}
+  for(var i=0;i<labels.length;i++){
+    if(patt.test(labels[i].firstChild.data)){
+      searchTree = labels[i].parentNode.parentNode.Tree
+      searchTreeArr.push(searchTree)//以便还原
+      searchTree.labelRender(true,patt)
+      openToRoot(searchTree)
+      //打开父级直到root
+      function openToRoot(searchTree){
+        if(searchTree.parent){
+          if(searchTree.parent.iOpen.classList.contains("img-close")){searchTree.parent.toggleChilds()}
+          openToRoot(searchTree.parent)
+        }
+      }
+    }
+  }
+  if(!searchTree){alert("未找到所求值")}
+}
+
+// 绑定点击事件
+var rootNode=new Tree(document.getElementById("root"))
+rootNode.selfNode.onclick=function(event){
+  if(event.target.parentNode.tagName==="P"){
+    var clickedTree = event.target.parentNode.parentNode.Tree //dom节点返回Tree
+    if(event.target.classList.contains("img-add")){
+      var pro =prompt("请输入新子节点名")
+      if(pro&&pro.trim()){clickedTree.addChild(pro)}
+    }
+    if(event.target.classList.contains("img-delete")){
+      clickedTree.delSelf(confirm("确认删去这个节点？"))
+    }
+    if(event.target.classList.contains('img-open')||event.target.tagName==="LABEL"){
+      clickedTree.toggleChilds()
+    }
+  }
+}
+
+//初始化
+rootNode.addChild("web","after","phone");
+rootNode.childs[0].addChild("html","css","js").toggleChilds()
+rootNode.childs[0].childs[1].addChild("sass").toggleChilds()
+rootNode.childs[0].childs[2].addChild("node","ajax","jquery").toggleChilds()
+rootNode.childs[1].addChild("C","php","java").toggleChilds()
+rootNode.childs[1].childs[0].addChild("C++","C#").toggleChilds()
+rootNode.childs[2].addChild("iOS","Android").toggleChilds()
